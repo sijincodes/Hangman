ed8ab4b1923c829bbe508a1152599e105d32f014
sijincodes <sijinmathews03@gmail.com>
2023-02-24 17:17:54 +0100

    added skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
-import './App.css'
+import words from "./wordList.json";
 
 function App() {
-
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetter, setGuessedLetter] = useState<string[]>([]);
 
   return (
-    <div className="App">
-      hi
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
