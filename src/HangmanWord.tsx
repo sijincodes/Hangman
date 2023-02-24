d2fdd6dc0951abbcd29e521c11ede13942dc9092
sijincodes <sijinmathews03@gmail.com>
2023-02-24 17:32:35 +0100

    word done

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function HangmanWord() {
+  const word="sijin"
+  const guessedLetters=['i', "m"]
   return (
-    <div>HangmanWord</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >{
+      word.split('').map((elm,index)=>
+        <span key={index} style={{borderBottom:"0.1em solid black"}}>
+          <span style={{visibility:guessedLetters.includes(elm)?"visible" : "hidden"}}>{elm}</span>
+        </span>
+      )
+    }</div>
+  );
 }
 
-export default HangmanWord
+export default HangmanWord;
